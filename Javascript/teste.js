69c0cf1f1465b7f47dcabbe585ec313b534867ee
Letícia Pinheiro <leticiapinheiro3137@outlook.com>
2023-03-27 19:36:47 -0300

    web

---
+++ Javascript/teste.js
@@ -0,0 +1,31 @@
+/*function returnEvenValues(array){// ver numeros pares na lista 
+    let evenNums = [];
+    for(let i = 0; i < array.length; i++){
+        if(array[i] % 2 === 0){
+            evenNums.push(array[i]);
+        } else{
+            console.log(`${array[i]} não é par`)
+        }
+    }
+
+    console.log('Os numeros pares: ' + evenNums);
+}
+
+let array = [1, 2, 3, 4, 5, 6]
+
+returnEvenValues(array) */
+
+var palavra = '9'
+var numero = 9
+
+if (palavra === numero) {
+    console.log('true');
+} else {
+    console.log('false')
+}
+
+var n1 = 4
+var n2 = 5
+var soma = n1 + n2;
+console.log(soma);
+
