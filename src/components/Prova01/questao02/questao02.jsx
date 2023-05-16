c8d1e8aa8847bc8dffdc48aebcb2c6d695d26ed2
Letícia Pinheiro <leticiapinheiro3137@outlook.com>
2023-05-16 09:00:53 -0300

    prova

--- src/components/Prova01/questao02/questao02.jsx
+++ src/components/Prova01/questao02/questao02.jsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import frente from "./frente.png"
 import costa from "./costas.png";
 const Questao02 = () => {
-    //variavel para verificar se a carta está virada ou não
+    //variavel para verificar se o pokemon está virado ou não
     const [virou, setVirou] = useState(false);
     //variavel para verificar qual imagem será exibida
     const [img, setImg] = useState(frente);
 
-    //função para virar a carta
+    //função para virar o pokemon
     const virar = () => {
         console.log("ola");
         if (virou == false) {
